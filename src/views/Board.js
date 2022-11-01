39917794fcf44c31ea42ec74eb80e58818353b14
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-31 23:59:51 -0600

    WIP: add color input

--- src/views/Board.js
+++ src/views/Board.js
@@ -1,5 +1,16 @@
 import styles from './Board.module.css';
 
 export default function Board() {
-  return <div className={styles.container}>Board</div>;
+  let value = '#e66465';
+
+  return (
+    <div className={styles.container}>
+      <h2>Board</h2>
+      <form>
+        <p>Choose your monster's color:</p>
+        <input type="color" id="head" name="head" value={value} />
+        <label for="head">Head</label>
+      </form>
+    </div>
+  );
 }
