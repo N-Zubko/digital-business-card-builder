82704ed3f5f6d35017393c08af8dacaef6118825
Nadia Zubko <ne.nadezda@gmail.com>
2022-11-01 12:01:21 -0600

    add ProfilePicture to Canvas

--- src/views/Canvas.js
+++ src/views/Canvas.js
@@ -4,10 +4,12 @@ import Info from '../components/Info';
 import About from '../components/About';
 import Interest from '../components/Interest';
 import Footer from '../components/Footer';
+import ProfilePicture from '../components/ProfilePicture';
 
 export default function Canvas() {
   return (
     <div className={styles.container}>
+      <ProfilePicture />
       <Info />
       <About />
       <Interest />
