6bbf18967fedb03c7fa226d1acba51e8f3d8c864
Nadia Zubko <ne.nadezda@gmail.com>
2022-11-01 13:59:04 -0600

    add Context to Info, add userTitle, userName, website, email, linkedin to context

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,18 +1,20 @@
 import styles from './Info.module.css';
+import { useContext } from 'react';
+import { UserContext } from '../utils/Context';
 import emailLogo from '../assets/Email-logo.png';
 import linkendinLogo from '../assets/LinkedIn-logo.png';
 
 export default function Info() {
-  const name = 'Nadia Zubko';
-  const email = 'mailto:na-zubko@outlook.com';
-  const linkedIn = 'https://www.linkedin.com/in/nadezhda-zubko-developer/';
-  const title = 'Full Stack Web Developer';
-  const website = 'website coming soon';
+  const { userTitle, userName, website, email, linkedIn, titleColor } =
+    useContext(UserContext);
+
   return (
     <div className={styles.container}>
       <div className={styles.bio}>
-        <h1 className={styles.name}>{name}</h1>
-        <p className={styles.title}>{title}</p>
+        <h1 className={styles.name}>{userName}</h1>
+        <p className={styles.title} style={{ color: `${titleColor}` }}>
+          {userTitle}
+        </p>
         <p className={styles.small}>{website}</p>
         <div className={styles.contacts}>
           <a
