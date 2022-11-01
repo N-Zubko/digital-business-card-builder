4d0858c21363c3fe284518523e6147f8f88a3c8e
Nadia Zubko <ne.nadezda@gmail.com>
2022-11-01 15:36:13 -0600

    Change about text

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import styles from './About.module.css';
 
 export default function About() {
   const text =
-    'I am a translator who turned to become full stack web developer. Helping people to communicate their needs, have their best web experience, polishing every detail, and learning new technologies - the things I am best at';
+    'I am a translator who decided to become a full stack web developer. Helping people to communicate their needs, provide them with seemless web experience, polishing every detail, and learning new technologies are the things I am best at.';
   return (
     <main className={styles.about}>
       <h2 className={styles.header}>About</h2>
